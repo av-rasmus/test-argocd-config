eacfb5c5a7fa29c962be30916df248b51535f007
Rasmus Gormsbøl <rasmus.gormsboel@assemblyvoting.com>
2025-04-04 13:24:40 +0200

    add repo command

--- clusters/bootstrap/install-argo.ps1
+++ clusters/bootstrap/install-argo.ps1
@@ -1,3 +1,5 @@
 kubectl create namespace argocd
 
 kubectl apply -n argocd -f https://raw.githubusercontent.com/argoproj/argo-cd/stable/manifests/install.yaml
+
+argocd repo add https://github.com/av-rasmus/test-argocd-config.git
